819c60804852ad5455ea7e2eeed3787fb43efb8b
Yu YAGIHASHI <yagihash@ssrf.dev>
2025-03-29 12:12:53 +0900

    implement qi list

--- cmd/qi/main.go
+++ cmd/qi/main.go
@@ -20,6 +20,16 @@ const (
 
 type listCmd struct {
 	client *client.Client
+
+	page          int
+	perPage       int
+	sort          string
+	direction     string
+	visibility    string
+	createdAfter  string
+	createdBefore string
+	updatedAfter  string
+	updatedBefore string
 }
 
 func (*listCmd) Name() string     { return "list" }
@@ -30,11 +40,41 @@ func (*listCmd) Usage() string {
 `
 }
 
-func (l *listCmd) SetFlags(f *flag.FlagSet) {}
+func (l *listCmd) SetFlags(f *flag.FlagSet) {
+	// TODO:validate values
+	f.IntVar(&l.page, "page", 0, "page number")
+	f.IntVar(&l.perPage, "per-page", 0, "per-page")
+	f.StringVar(&l.sort, "sort", "", "sort field [created, updated]")
+	f.StringVar(&l.direction, "direction", "", "direction field [asc, desc]")
+	f.StringVar(&l.visibility, "visibility", "", "visibility field [MYSELF, ANYONE, URL_ONLY]")
+	f.StringVar(&l.createdAfter, "created-after", "", "filter with created field in ISO8601 format")
+	f.StringVar(&l.createdBefore, "created-before", "", "filter with created field in ISO8601 format")
+	f.StringVar(&l.updatedAfter, "updated-after", "", "filter with updated field in ISO8601 format")
+	f.StringVar(&l.updatedBefore, "updated-before", "", "filter with updated field in ISO8601 format")
+}
 
 func (l *listCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...any) subcommands.ExitStatus {
-	res, _ := l.client.ListPosts(nil)
-	data, _ := json.MarshalIndent(res, "", "  ")
+	params := &client.ListPostsParams{
+		Page:          l.page,
+		PerPage:       l.perPage,
+		Sort:          l.sort,
+		Direction:     l.direction,
+		Visibility:    l.visibility,
+		CreatedAfter:  l.createdAfter,
+		CreatedBefore: l.createdBefore,
+		UpdatedAfter:  l.updatedAfter,
+		UpdatedBefore: l.updatedBefore,
+	}
+	res, err := l.client.ListPosts(params)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return subcommands.ExitFailure
+	}
+	data, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return subcommands.ExitFailure
+	}
 	fmt.Printf("%s\n", data)
 	return subcommands.ExitSuccess
 }
